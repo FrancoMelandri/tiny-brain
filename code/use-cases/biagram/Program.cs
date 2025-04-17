896ae7d2087d90082d3c46366c1d860677df5923
FrancoMelandri <franco.melandri@gmail.com>
2025-04-17 12:13:33 +0200

    adding loss function as minimizing ngative log likelihood

--- code/use-cases/biagram/Program.cs
+++ code/use-cases/biagram/Program.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using TinyFp.Extensions;
 
 //
@@ -19,10 +20,10 @@ words.Max(_ => _.Length).Tee(_ => Console.WriteLine($"Max: {_}"));
 //
 // character sequence
 //
-// create a list of tuple containing two character the previsou and the next
+// create a list of tuple containing two character the previous and the next
 // adding two special characters at the beginning and the end of the string load form file
-var chars = words
-    // .Take(3)
+IEnumerable<(char Char1, char Char2)> ComputeBiagrams(string[] words) =>
+    words
     .Select(_ => $"`{_}`")
     .Select(
         w =>
@@ -33,20 +34,24 @@ var chars = words
          (a, i) => a.Tee(_ => _.AddRange(i.Select(_ => (_.Char1, _.Char2)))));
 
 
+var biagrams = ComputeBiagrams(words);
+
 //
 // count all occurence for each tuple (c1, c2)
-var dictionary = chars.Fold(new Dictionary<(char, char), int>(),
+var dictionary = biagrams.Fold(new Dictionary<(char, char), int>(),
     (a, i) =>
         a.ToOption(_ => !_.ContainsKey(i))
             .Match(_ => a.Tee(__ => a[i] += 1),
                 () => a.Tee(__ => a[i] = 1)));
 
+//
+// convert character to index and vice versa
 Func<char, int> CtoI = c => (int)c - (int)'`';
 Func<int, char> ItoC = i => (char)(i + (int)'`');
 
 //
 // initialize a matrix containing the sum of the second character following the previous character
-var N = chars.Fold(new int[27, 27],
+var N = biagrams.Fold(new int[27, 27],
     (a, i) => a.Tee(_ => a[CtoI(i.Item1), CtoI(i.Item2)] += 1));
 
 //
@@ -63,7 +68,6 @@ for (var i = 0; i < 27; ++i)
 //
 // generate characters using multinomial
 Console.WriteLine("Generate:");
-
 for (var toGenerate = 0; toGenerate < 10; toGenerate++)
 {
     var ix = 0;
@@ -90,5 +94,31 @@ for (var toGenerate = 0; toGenerate < 10; toGenerate++)
     var generateString = generated.Fold(string.Empty, (a, c) => a.Tee(_ => _ + c));
     Console.WriteLine(generateString);
 }
+
+//
+// define a function to estimate the the model prediction
+// we can consider the likelihood to estimate the parameter
+// likelihood = Product(pi)
+// we can consider the log
+// log(likelihood) = sum ( log(pi) )
+//
+// due the fact the loss function should be close to zero when is good, 
+// we can define our loss funciton as
+// loss = -log(likelihood)/N
+// where N is to normalize
+
+double ComputeLoss(string[] words, double[,] P, Func<char, int> CtoI) 
+    => ComputeBiagrams(words)
+    .Fold((0.0, 0),
+        (a, i) => 
+            (
+                a.Item1 + Math.Log(P[CtoI(i.Item1), CtoI(i.Item2)]), 
+                a.Item2 + 1
+                ))
+    .Map(_ => -_.Item1 / _.Item2);;
+
+var loss = ComputeLoss(words, P, CtoI);
+Console.WriteLine($"Loss: {loss}");
+
 
 
